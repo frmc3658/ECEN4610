ab9050c020e20a21421171f68a2a03dea8e714bf
dale4990 <dale4990@colorado.edu>
2023-10-26 16:57:01 +0000

    initial test scripts for OpenCV and Camera Usage

---
+++ video_test.py
@@ -0,0 +1,27 @@
+import cv2
+
+# Create a VideoCapture object to access the camera (use 0 for the default camera)
+cap = cv2.VideoCapture(0)
+
+# Check if the camera was opened successfully
+if not cap.isOpened():
+    print("Error opening the camera.")
+else:
+    while True:
+        # Read a frame from the camera
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Error reading a frame.")
+            break
+
+        # Display the frame in a window
+        cv2.imshow('Camera Feed', frame)
+
+        # Break the loop if the 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the camera and close the OpenCV window
+    cap.release()
+    cv2.destroyAllWindows()
