080b8cf3f4e0099fedcba1e57c797594616f5d75
frmc3658 <frmc3658@colorado.edu>
2024-03-12 19:45:54 -0600

    refactored 2D stepper code

--- Arduino/2D_Track.ino
+++ Arduino/2D_Track.ino
@@ -1,213 +1,314 @@
+/* C++ STD Headers */
+#include <array>
+/* Arudunio Headers */
 #include "esp32-hal-timer.h"
+/* Application headers */
 
+
+/******************************************/
+/*           MACRO DEFINITIONS            */
+/******************************************/
+
+/* Pin Definitions: Steppers  */
+#define STEP_PIN_AZ             15
+#define DIR_PIN_AZ              3
+#define STEP_PIN_ALT            36
+#define DIR_PIN_ALT             37
+/* Step Size Definitions */
+#define FULL_STEP               {LOW, LOW, LOW}     /* MS1, MS2, MS3: drive all low  */
+#define HALF_STEP               {HIGH, LOW, LOW}    /* MS1: drive high; MS2, MS3: drive low */
+#define QUARTER_STEP            {LOW, HIGH, LOW}    /* MS1: drive low; MS2: drive high; MS3: drive low */
+#define EIGHTH_STEP             {HIGH, HIGH, LOW}   /* MS1, MS2: drive high; MS3: drive low */
+#define SIXTEENTH_STEP          {HIGH, HIGH, HIGH}  /* MS1, MS2, MS3: drive all high */
+/* Pin Definitions: Driver */
+#define MS1_PIN                 7
+#define MS2_PIN                 5
+#define MS3_PIN                 6
+/* Serial Definitions */
+#define SERIAL_BAUDE            115200
+#define SERIAL_UART_RX          18
+#define SERIAL_UART_TX          17
+/* Clock Definitions */
+#define TIMER_CLK_DIV           40
+#define ATTACH_TIMER_ALARM      1000000
+#define ATTACH_INT_ON_EDGE      true
+#define TIMER_COUNT_UP          true
+#define TIMER_INT_AUTO_RELOAD   true
+#define MAX_TIMER_INTERVAL      250000
+
+
+/******************************************/
+/*           FORWARD DECLARATIONS         */
+/******************************************/
+
+int calculatePID(double kp, double ki, double kd, double* integralXY, int const currentError, double* previousError);
+void controlStepper(hw_timer_t* stepperTimer, int directionPin, int pidOutput);
+/* MACRO functions for easier use */
+#define CALCULATE_PID_X(DIST_X)           calculatePID(Kp_az, Ki_az, Kd_az, &integralX, DIST_X, &previousErrorX)
+#define CALCULATE_PID_Y(DIST_Y)           calculatePID(Kp_alt, Ki_alt, Kd_alt, &integralX, DIST_Y, &previousErrorY)
+#define CONTROL_AZ_STEPPER(PID_OUTPUT)    controlStepper(azTimer, DIR_PIN_AZ, PID_OUTPUT)
+#define CONTROL_ALT_STEPPER(PID_OUTPUT)   controlStepper(altTimer, DIR_PIN_ALT, PID_OUTPUT)
+
+
+/******************************************/
+/*           GLOBAL VARIABLES             */
+/******************************************/
+
+// Stepper tick flags
 volatile bool azTick = false;
 volatile bool altTick = false;
 
+// Timers for the alt/az stepper motors
 hw_timer_t* azTimer = NULL;
 hw_timer_t* altTimer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
-
-// Pin Definitions
-#define stepPinAZ 15
-#define dirPinAZ 3
-#define stepPinALT 36
-#define dirPinALT 37
-//#define potPin A5
-#define MS1pin 7
-#define MS2pin 5
-#define MS3pin 6
-
-static int MAX_INTERVAL = 250000;
-
 // PID variables: ALT (adjust these to fine-tune)
-double Kp_alt = 0.5;  // Proportional gain
-double Ki_alt = 0.0005;  // Integral gain
+double Kp_alt = 0.5;    // Proportional gain
+double Ki_alt = 0.0005; // Integral gain
 double Kd_alt = 0.005;  // Derivative gain
 
 // PID variables: AZ (adjust these to fine-tune)
 double Kp_az = 0.55;  // Proportional gain
-double Ki_az = 0.005;  // Integral gain
+double Ki_az = 0.005; // Integral gain
 double Kd_az = 0.05;  // Derivative gain
 
-int k = 15;
+// Error variables
+double previousErrorX   = 0;
+double integralX        = 0;
+double previousErrorY   = 0;
+double integralY        = 0;
 
-double previousErrorX = 0;
-double integralX = 0;
-double previousErrorY = 0;
-double integralY = 0;
+int setPoint        = 0;
+bool lastDirection  = 0;
+int scanCounter     = 0;
 
-int setPoint = 0;
 
-bool lastDirection = 0;
-int scanCounter = 0;
+/******************************************/
+/*        INTERRUPT SERVICE ROUTINES      */
+/******************************************/
 
-void IRAM_ATTR AzTimer() {
+
+/*
+ * @brief Azimuth stepper timer ISR
+ */
+void IRAM_ATTR AzTimer()
+{
+  // Enter critical section
   portENTER_CRITICAL_ISR(&timerMux);
-  //Serial.println("Az Tick!");
+
+  // Drive signal to the azimuth stepper
   azTick = !azTick;
-  digitalWrite(stepPinAZ, azTick ? HIGH : LOW);
+  digitalWrite(STEP_PIN_AZ, azTick ? HIGH : LOW);
+
+  // Exit critical section
   portEXIT_CRITICAL_ISR(&timerMux);
 }
 
-void IRAM_ATTR AltTimer() {
+
+/*
+ * @brief Altitude stepper timer ISR
+ */
+void IRAM_ATTR AltTimer()
+{
+  // Enter critical section
   portENTER_CRITICAL_ISR(&timerMux);
-  //Serial.println("Alt Tick!");
+
+  // Drive signal to the altitude stepper
   altTick = !altTick;
-  digitalWrite(stepPinALT, altTick ? HIGH : LOW);
+  digitalWrite(STEP_PIN_ALT, (altTick ? HIGH : LOW));
+
+  // Exit critical section
   portEXIT_CRITICAL_ISR(&timerMux);
 }
 
-void setup() {
-  Serial.begin(115200);
-  Serial1.begin(115200, SERIAL_8N1, 18, 17);
-  // Set pins as Outputs
-  pinMode(stepPinAZ, OUTPUT);
-  pinMode(dirPinAZ, OUTPUT);
-  pinMode(stepPinALT, OUTPUT);
-  pinMode(dirPinALT, OUTPUT);
 
-  pinMode(MS1pin, OUTPUT);
-  pinMode(MS2pin, OUTPUT);
-  pinMode(MS3pin, OUTPUT);
+/******************************************/
+/*        ARDUINO SETUP/MAIN LOOP         */
+/******************************************/
 
-  
-  // setupAzTimer(1000000);
-  azTimer = timerBegin(0, 40, true); 
-  //Serial.println("HereAZ1");
-  timerAttachInterrupt(azTimer, &AzTimer, true);
-  //Serial.println("HereAZ"); 
-  timerAlarmWrite(azTimer, 1000000, true);
+/*
+ * @brief Arduino Setup
+ */
+void setup()
+{
+  // Set serial port baud rates
+  // NOTE: Serial port is the standard serial monitor for debugging.
+  //       Serial1 port is the UART pin connected to the raspberry pi,
+  //       which is used as input to drive the stepper motors in order
+  //       to track the marker.
+  Serial.begin(SERIAL_BAUDE);
+  Serial1.begin(SERIAL_BAUDE, SERIAL_8N1, SERIAL_UART_RX, SERIAL_UART_TX);
+
+  // Set alt/az stepper pins as Output
+  pinMode(STEP_PIN_AZ, OUTPUT);
+  pinMode(DIR_PIN_AZ, OUTPUT);
+  pinMode(STEP_PIN_ALT, OUTPUT);
+  pinMode(DIR_PIN_ALT, OUTPUT);
+
+  // Set stepper driver pins as output
+  pinMode(MS1_PIN, OUTPUT);
+  pinMode(MS2_PIN, OUTPUT);
+  pinMode(MS3_PIN, OUTPUT);
+
+  // Setup the azimuth timer
+  azTimer = timerBegin(0, TIMER_CLK_DIV, TIMER_COUNT_UP);
+  timerAttachInterrupt(azTimer, &AzTimer, ATTACH_INT_ON_EDGE);
+  timerAlarmWrite(azTimer, ATTACH_TIMER_ALARM, TIMER_INT_AUTO_RELOAD);
   timerAlarmEnable(azTimer);
   
-  // setupAltTimer(1000000);
-  altTimer = timerBegin(1, 40, true);
-  timerAttachInterrupt(altTimer, &AltTimer, true);
-  timerAlarmWrite(altTimer, 1000000/*intervalAlt*/, true);
-  //Serial.println("HereALT"); 
+  // Setup the altitude timer
+  altTimer = timerBegin(1, TIMER_CLK_DIV, TIMER_COUNT_UP);
+  timerAttachInterrupt(altTimer, &AltTimer, ATTACH_INT_ON_EDGE);
+  timerAlarmWrite(altTimer, ATTACH_TIMER_ALARM, TIMER_INT_AUTO_RELOAD);
   timerAlarmEnable(altTimer);
-  
 }
 
-void loop() {
-  if (Serial1.available() > 0) {
+
+/*
+ * @brief Arduino main loop
+ */
+void loop()
+{
+  // Process serial UART input data from raspberry pi
+  // NOTE: If there is data available, then that means OpenCV can
+  //       find the marker
+  if(Serial1.available() > 0)
+  {
+    // Pasrse distance coordinate data received from the raspberry pi
+    // over the serial UART port
     String data = Serial1.readStringUntil('\n');
     int dist_x = data.substring(0, data.indexOf(',')).toInt();
     int dist_y = data.substring(data.indexOf(',') + 1).toInt();
 
+    // Print data to the serial monitor
     Serial.print("Received: X: ");
     Serial.print(dist_x);
     Serial.print(", Y: ");
     Serial.println(dist_y);
 
     // Calculate PID output
-    int pidOutputAZ = calculatePIDX(dist_x);
-    int pidOutputALT = calculatePIDY(dist_y);
+    int pidOutputAZ = CALCULATE_PID_X(dist_x);
+    int pidOutputALT = CALCULATE_PID_Y(dist_y);
 
     // Control stepper motor based on PID output
-    controlStepperAZ(pidOutputAZ);
-    controlStepperALT(pidOutputALT);
+    CONTROL_AZ_STEPPER(pidOutputAZ);
+    CONTROL_ALT_STEPPER(pidOutputALT);
   }
-  //  else{
-  //    findMarkerX();
-  //  }
+  // else /* Marker has been lost */
+  // {
+  //   findMarkerX();
+  // }
 }
 
-// Function to calculate PID output
-int calculatePIDX(int currentError) {
-  double errorX = setPoint - currentError;
-  integralX += errorX;
+/******************************************/
+/*          FUNCTION DEFINITIONS          */
+/******************************************/
 
-  // Anti-windup (optional): Limit the integral term
-  if (integralX > 100) {
-    integralX = 100;
-  } else if (integralX < -100) {
-    integralX = -100;
-  }
+/*
+ * @brief Calculates PID error
+ *
+ * @param[in] kp  Proportional gain
+ * @param[in] ki  Integral gain
+ * @param[in] kd  Derivative gain
+ * @param[in] integralXY  Previous x or y integral term
+ * @param[in] currentError  The current error based on feedback loop
+ * @param[in] previousError Previously calculated PID error
+ * 
+ * @return Step size based on output of PID loop
+ * 
+ * @note MACRO functions provided for ease of use
+ */
+int calculatePID(double kp, double ki, double kd, double* integralXY, 
+                 int const currentError, double* previousError)
+{
+  // Calculate error and adjust integral term
+  double error = setPoint - currentError;
+  *integralXY += error;
 
-  double derivativeX = errorX - previousErrorX;
+  // Anti-windup: Limit the integral term
+  if(*integralXY > 100){ *integralXY = 100; }
+  else if(*integralXY < -100){ *integralXY = -100; }
+
+  // Calculate derivative
+  double derivative = error - *previousError;
 
   // PID formula
-  double outputX = Kp_az * errorX + Ki_az * integralX + Kd_az * derivativeX;
+  double output = kp * error + ki * (*integralXY) + kd * derivative;
 
   // Save current error for the next iteration
-  previousErrorX = errorX;
+  *previousError = error;
 
   // Convert the output to a step size
-  return static_cast<int>(outputX);
+  return static_cast<int>(output);
 }
 
-// Function to calculate PID output
-int calculatePIDY(int currentError) {
-  double errorY = setPoint - currentError;
-  integralY += errorY;
 
-  // Anti-windup (optional): Limit the integral term
-  if (integralY > 100) {
-    integralY = 100;
-  } else if (integralY < -100) {
-    integralY = -100;
+/*
+ * @brief Signals the
+ *
+ * @param[in] stepSize  Array of signals to output to the driver
+ * 
+ * @note  Macros are provided for stepSize input param
+ *        for ease of use
+ */
+void driveStepper(std::array<int, 3> const &stepSize)
+{
+  digitalWrite(MS1_PIN, stepSize[0]);
+  digitalWrite(MS2_PIN, stepSize[1]);
+  digitalWrite(MS3_PIN, stepSize[2]);
+}
+
+
+/*
+ * @brief Controls one of the stepper motors based on frequency
+ *
+ * @param[in] stepperTimer  The timer associated the stepped to control
+ * @param[in] directionPin  Direction control pin of the stepper
+ * @param[in] pidOutput     Output of the PID loop
+ * 
+ * @note  Maco functions are provided for use with the alt/az steppers
+ *        for ease of use
+ */
+void controlStepper(hw_timer_t* stepperTimer, int directionPin, int pidOutput)
+{
+  driveStepper(SIXTEENTH_STEP);
+  
+  digitalWrite(directionPin, (pidOutput >= 0) ? HIGH : LOW);
+  
+  if(pidOutput < 4)
+  {
+    digitalWrite(directionPin, LOW);
   }
 
-  double derivativeY = errorY - previousErrorY;
+  // Calculate stepper frequency and interval
+  float stepperFreq = (abs(pidOutput) / 100.0) * 500 * 4;  
+  float stepperInterval = (stepperFreq > 0) ? 1000000 / stepperFreq : MAX_TIMER_INTERVAL;
+  
+  // Print stepper frequency to serial monitor
+  Serial.print("Stepper Frequency: ");
+  Serial.println(stepperFreq);
 
-  // PID formula
-  double outputY = Kp_alt * errorY + Ki_alt * integralY + Kd_alt * derivativeY;
-
-  // Save current error for the next iteration
-  previousErrorY = errorY;
-
-  // Convert the output to a step size
-  return static_cast<int>(outputY);
+  // Change the stepper interrupt alarm
+  timerAlarmWrite(stepperTimer, stepperInterval, TIMER_INT_AUTO_RELOAD);
 }
 
-// Function to control the stepper motor based on the frequency
-void controlStepperAZ(int pidOutputAZ) {
-  sixteenStep();
-  
-  digitalWrite(dirPinAZ, (pidOutputAZ >= 0) ? HIGH : LOW);
-  
-  if(pidOutputAZ < 4){
-    digitalWrite(stepPinAZ, LOW);
-  }
-  float k = 0.5;
-  //float azFreq = 600 * (1 - exp(-k * abs(pidOutputAZ))); // Calculate azimuth frequency
-  float azFreq = ((abs(pidOutputAZ) / 100.0) * 500)*4;  
-  float intervalAZ = (azFreq > 0) ? 1000000 / azFreq : MAX_INTERVAL;
-  Serial.print("Az Frequency: ");
-  Serial.println(azFreq);
-
-  timerAlarmWrite(azTimer, intervalAZ, true);
-}
-
-void controlStepperALT(int pidOutputALT) {
-  
-  digitalWrite(dirPinALT, (pidOutputALT >= 0) ? HIGH : LOW);
-
-  if(pidOutputALT < 4){
-    // setupAltTimer(5000000);
-    digitalWrite(stepPinALT, LOW);
-   }
-  float k = 0.5;    
-  //float altFreq = 600 * (1 - exp(-k * abs(pidOutputALT)));     
-  float altFreq = (abs(pidOutputALT) / 100.0) * 500 * 4; // Linearly maps 0-100 to 0-400Hz
-
-  float intervalALT = (altFreq > 0) ? 1000000 / altFreq : MAX_INTERVAL;
-  Serial.print("Alt Freq: ");
-  Serial.println(altFreq);
-  timerAlarmWrite(altTimer, intervalALT, true);
-}
-
-
-void findMarkerX() {
+/*
+ * @brief Drives steppers to scan for the marker when not detected
+ */
+void findMarkerX()
+{
   // Adjust scanning parameters based on scan counter
-  int tick = 16; // Default step size
+  int tick = 16;  // Default step size
   int len = 1200; // Default step duration
 
-  if (scanCounter > 50 && scanCounter < 100) {
+  if(scanCounter > 50 && scanCounter < 100)
+  {
     // Reverse direction for smoother scanning
-    digitalWrite(dirPinAZ, !lastDirection);
-  } else if (scanCounter >= 100) {
+    digitalWrite(DIR_PIN_AZ, !lastDirection);
+  }
+  else if(scanCounter >= 100)
+  {
     // Marker not found within reasonable scans, handle error or fallback
     // stop scanning and return to main loop
     scanCounter = 0; // Reset scan counter
@@ -215,46 +316,15 @@ void findMarkerX() {
   }
 
   // Pulse StepPin
-  for (int i = 0; i < abs(tick); ++i) {
-    digitalWrite(stepPinAZ, HIGH);
+  for(int i = 0; i < abs(tick); ++i)
+  {
+    digitalWrite(STEP_PIN_AZ, HIGH);
     delayMicroseconds(len);
-    digitalWrite(stepPinAZ, LOW);
+    digitalWrite(STEP_PIN_AZ, LOW);
     delayMicroseconds(len);
   }
 
   // Update scanning parameters and counters
   lastDirection = !lastDirection; // Toggle direction for next scan
   scanCounter++;
-}
-
-
-// Custom Functions for Step Sizes
-void fullStep() {
-  digitalWrite(MS1pin, LOW);
-  digitalWrite(MS2pin, LOW);
-  digitalWrite(MS3pin, LOW);
-}
-
-void halfStep() {
-  digitalWrite(MS1pin, HIGH);
-  digitalWrite(MS2pin, LOW);
-  digitalWrite(MS3pin, LOW);
-}
-
-void quarterStep() {
-  digitalWrite(MS1pin, LOW);
-  digitalWrite(MS2pin, HIGH);
-  digitalWrite(MS3pin, LOW);
-}
-
-void eightStep() {
-  digitalWrite(MS1pin, HIGH);
-  digitalWrite(MS2pin, HIGH);
-  digitalWrite(MS3pin, LOW);
-}
-
-void sixteenStep() {
-  digitalWrite(MS1pin, HIGH);
-  digitalWrite(MS2pin, HIGH);
-  digitalWrite(MS3pin, HIGH);
 }
