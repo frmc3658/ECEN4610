31ae78fd6c417bd458fa27b33cab9316453dac46
frmc3658 <frmc3658@colorado.edu>
2024-04-15 10:21:24 -0600

    multiple detectors

--- ARuCo/aruco_test.py
+++ ARuCo/aruco_test.py
@@ -19,8 +19,8 @@ ser = serial.Serial('/dev/ttyS0', 115200)
 
 min_marker_size = 27
 max_marker_size = 440
-min_offset = 10
-max_offset = 30
+min_offset = 0
+max_offset = 10
 
 marker_size_range = max_marker_size - min_marker_size
 offset_range = max_offset - min_offset
@@ -64,43 +64,57 @@ else:
                     print("Marker not detected!")
                     start_time_scan = time.time()
             else:
+                
+                # Initialize variables to keep track of longest top edge
+                longest_top_edge_length = 0
+                longest_top_edge_index = None
+                
+                previous_marker_index = None
+                
                 # Find the marker with ID 69
                 for i in range(len(ids)):
-                    if ids[i] == 10:
-                        print("Marker detected!")
-                        # Draw the detected marker
-                        aruco.drawDetectedMarkers(frame, corners)
-                        
-                        marker_size = cv2.norm(corners[i][0][0] - corners[i][0][1])
-                        
-                        print(marker_size)
-                        
-                        y_offset = int((marker_size - min_marker_size) / marker_size_range * offset_range) + min_offset
-                        
-                        # Calculate the center of the crosshair
-                        crosshair_x = int(screenWidth / 2)
-
-                        crosshair_y = int((1*screenHeight/ 4) + y_offset) 
+                    # Calculate the length of the top edge
+                    top_edge_length = cv2.norm(corners[i][0][0] - corners[i][0][1])
                     
+                    if top_edge_length > longest_top_edge_length or top_edge_length == longest_top_edge_length:
+                        longest_top_edge_length = top_edge_length
+                        longest_top_edge_index = i
                         
-                        cv2.line(frame, (crosshair_x, crosshair_y - 10), (crosshair_x, crosshair_y + 10), (0, 255, 0), 2)  # crosshair
-                        cv2.line(frame, (crosshair_x - 10, crosshair_y), (crosshair_x + 10, crosshair_y), (0, 255, 0), 2)  # crosshair
-                                 
-                        # Calculate the center of the detected marker
-                        marker_center_x = int((corners[i][0][0][0] + corners[i][0][2][0]) / 2)
-                        marker_center_y = int((corners[i][0][0][1] + corners[i][0][2][1]) / 2)
+                previous_marker_index = longest_top_edge_index
+                
+                if previous_marker_index is not None:
+                    
+                    i = previous_marker_index
+                    print("Marker detected!")
+                    # Draw the detected marker
+                    aruco.drawDetectedMarkers(frame, corners) 
+                    
+                    y_offset = int((top_edge_length - min_marker_size) / marker_size_range * offset_range) + min_offset
+                    
+                    # Calculate the center of the crosshair
+                    crosshair_x = int(screenWidth / 2)
 
-                        # Draw a line from the crosshair to the marker
-                        cv2.line(frame, (crosshair_x, crosshair_y), (marker_center_x, marker_center_y), (255, 0, 0), 2)
-                        
-                        dist_x = marker_center_x - crosshair_x
-                        dist_y = marker_center_y - crosshair_y
-                        
-                        cv2.putText(frame, f"X: {dist_x}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-                        cv2.putText(frame, f"Y: {dist_y}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-                        
-                        data_to_send = f"{dist_x},{dist_y}\n"
-                        ser.write(bytes(data_to_send, 'utf-8'))
+                    crosshair_y = int((1*screenHeight/ 4) + y_offset) 
+                
+                    
+                    cv2.line(frame, (crosshair_x, crosshair_y - 10), (crosshair_x, crosshair_y + 10), (0, 255, 0), 2)  # crosshair
+                    cv2.line(frame, (crosshair_x - 10, crosshair_y), (crosshair_x + 10, crosshair_y), (0, 255, 0), 2)  # crosshair
+                             
+                    # Calculate the center of the detected marker
+                    marker_center_x = int((corners[i][0][0][0] + corners[i][0][2][0]) / 2)
+                    marker_center_y = int((corners[i][0][0][1] + corners[i][0][2][1]) / 2)
+
+                    # Draw a line from the crosshair to the marker
+                    cv2.line(frame, (crosshair_x, crosshair_y), (marker_center_x, marker_center_y), (255, 0, 0), 2)
+                    
+                    dist_x = marker_center_x - crosshair_x
+                    dist_y = marker_center_y - crosshair_y
+                    
+                    cv2.putText(frame, f"X: {dist_x}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                    cv2.putText(frame, f"Y: {dist_y}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                    
+                    data_to_send = f"{dist_x},{dist_y}\n"
+                    ser.write(bytes(data_to_send, 'utf-8'))
                         
                         
             cv2.imshow('Frame', frame)
